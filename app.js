78a7c83969a14c8aef59b3f4a512bb32a13af3f8
Martikainen <oliver.martikainen@cgi.com>
2019-11-13 18:05:29 +0200

    prob final version -testdata

--- app.js
+++ app.js
@@ -1,13 +1,16 @@
 const express = require('express')
 const app = express()
+const bodyParser = require('body-parser')
 //const port = process.env.PORT || 3010
-const http = require('http') 
+const http = require('http')
 const port = 3010
 const fs = require('fs')
 
+app.use(bodyParser.json())
+
 let count = 1
 const countUpdater = () => {
-    if(count === 5) {
+    if (count === 5) {
         count = 1
     } else {
         count++
@@ -17,7 +20,6 @@ const countUpdater = () => {
 const readTestData = (file) => {
     //const data = JSON.parse(fs.readFileSync(`./raw_data/${file}.json`, 'utf8'))
     const data = JSON.parse(fs.readFileSync(`./test_data/${file}.json`, 'utf8'))
-    //const data = JSON.parse(fs.readFileSync(`./public_data/${file}.json`, 'utf8'))
     console.log('GET ', file)
     return data
 }
@@ -33,13 +35,25 @@ app.post('/agents', (req, res) => res.send(readTestData('Agents')))
 app.post('/teams', (req, res) => res.send(readTestData('Teams')))
 app.post('/generalqueue', (req, res) => res.send(readTestData(`GeneralQueue${count}`)))
 app.post('/agentonlinestate', (req, res) => res.send(readTestData(`AgentsOnline${count}`)))
-app.post('/inboundreport', (req, res) => res.send(readTestData('InboundReport')))
+app.post('/inboundreport', (req, res) => { //body-parser needed for this
+    const type = req.body.ContactTypes
+    switch (type) {
+        case 'email':
+            res.send(readTestData(`InboundReportEmail${count}`))
+            break;
+        case 'PBX':
+            res.send(readTestData(`InboundReportPBX${count}`))
+            break;
+        default:
+            res.status(404).send(`check params ${body}`)
+    }
+})
 
 
 //app.listen(port, () => console.log(`Listening to port ${port}`))
 const server = http.createServer(app) //difference to just app.listen???
 
-server.listen(port, () => {console.log(`Test database server running from port ${port}`)})
+server.listen(port, () => { console.log(`Test database server running from port ${port}`) })
 
 
 setInterval(() => countUpdater(), 6000)
